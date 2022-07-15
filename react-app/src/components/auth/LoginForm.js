2239dd71960daa1a1baf2cd128e951e5a3dfe51f
Grace Chi <gracetrc7@gmail.com>
2022-07-15 00:24:43 -0500

    login validation

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect, NavLink } from 'react-router-dom';
 import { login } from '../../store/session';
@@ -6,8 +6,10 @@ import { login } from '../../store/session';
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
+  const [backendErrors, setBackendErrors] = useState([]);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
@@ -15,9 +17,20 @@ const LoginForm = () => {
     e.preventDefault();
     const data = await dispatch(login(email, password));
     if (data) {
-      setErrors(data);
+      // setErrors(data);
+      setBackendErrors(data);
+      setSubmitted(!submitted)
     }
   };
+
+  useEffect(() => {
+    const valErrors = [];
+    backendErrors.forEach(err => {
+      if (err === 'email : 1') valErrors.push('Email provided not found.')
+      if (err === 'password : 3') valErrors.push('Incorrect password and email.')
+    })
+    setErrors(valErrors)
+  }, [submitted])
 
   const updateEmail = (e) => {
     setEmail(e.target.value);
@@ -39,29 +52,34 @@ const LoginForm = () => {
         </div>
         <div>
           {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
+            <div className='error-messages' key={ind}>{error}</div>
           ))}
         </div>
         <div>
-          <label className='login-email-input' htmlFor='email'>Email</label>
+          {/* <label className='login-email-input' htmlFor='email'>Email</label> */}
           <input
             className='login-email-form-field'
             name='email'
             type='text'
-            // placeholder='Email'
+            placeholder='  Email'
             value={email}
             onChange={updateEmail}
+            required
+            autoComplete="off"
           />
         </div>
+
         <div>
-          <label className='login-password-input' htmlFor='password'>Password</label>
+          {/* <label className='login-password-input' htmlFor='password'>Password</label> */}
           <input
             className='login-password-form-field'
             name='password'
             type='password'
-            // placeholder='Password'
+            placeholder='  Password'
             value={password}
             onChange={updatePassword}
+            required
+            autoComplete="off"
           />
         </div>
         <div className='both-login-page-btns'>
@@ -70,7 +88,7 @@ const LoginForm = () => {
         </div>
         <div className='login-form-signup-nav'>
             Don't have an account?<span> </span>
-            <NavLink className='login-form-create-account' to='/sign-up'>Create an account</NavLink>
+            <NavLink className='login-form-create-account' to='/sign-up'>Sign up here!</NavLink>
         </div>
       </form>
     </div>
